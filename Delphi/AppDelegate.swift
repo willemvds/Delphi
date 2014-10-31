655e9cec66930fb23517ff14807b538b65712084
Willem van der Schyff <willemvds@gmail.com>
2014-10-31 19:16:58 +0200

    Initial Commit

---
+++ Delphi/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Delphi
+//
+//  Created by Willem van der Schyff on 2014/10/31.
+//  Copyright (c) 2014 Willem van der Schyff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
