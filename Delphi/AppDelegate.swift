78528da1840ccba0d7399f7b43d0ea2e37b4eec9
Willem van der Schyff <willemvds@gmail.com>
2014-11-02 15:45:45 +0200

    Poking around xcode like delphi back in the day

--- Delphi/AppDelegate.swift
+++ Delphi/AppDelegate.swift
@@ -7,15 +7,29 @@
 //
 
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet weak var window: NSWindow!
 
+    @IBOutlet var bottomTextView: NSTextView!
+
+    @IBOutlet weak var leftTableView: NSTableView!
+    @IBOutlet weak var rightTableView: NSTableView!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        var fileManager :NSFileManager = NSFileManager.defaultManager();
+        fileManager.changeCurrentDirectoryPath("/");
+        var error :NSError?
+        var path :String = "/Users/willemvds/Documents/"
+        var paths = fileManager.subpathsOfDirectoryAtPath(path, error: &error) as [NSString]
+        for path in paths {
+            println(path)
+            let attrString1 = NSAttributedString(string:path+"\n")
+            //leftTableView.insertText(path)
+            bottomTextView.textStorage?.appendAttributedString(attrString1)
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
